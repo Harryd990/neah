5203d6c3a0f343f49682c1df23ee8022a6bc6ee3
Harry Duff <duffhj@kingschester.co.uk>
2025-11-21 12:54:44 +0000

    fddsf\df\fd

---
+++ neah/neah/Grid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neah
+{
+    public class Grid
+    {
+        public int Width {get;}
+        public int Height {get;}
+        private string[,]cells;
+        public Grid(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            cells = new string[width, height];
+
+
+        }
+        public string GetCell(int x, int y)
+        {
+            return cells[x, y];
+        }
+    }
+}
