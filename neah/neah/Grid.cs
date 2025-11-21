efa6956941cf3efb30846f96eb67b95e60f94f23
Harry Duff <duffhj@kingschester.co.uk>
2025-11-21 13:26:29 +0000

    it frigging works (grid all set up)

--- neah/neah/Grid.cs
+++ neah/neah/Grid.cs
@@ -27,8 +27,42 @@ namespace neah
         public void SetCell(int x, int y, string value)
         {
             cells[x, y] = value;
-        }   
-
+        }
+        public void ClearCell(int x, int y)
+        {
+            cells[x, y] = null;
+        }
+        public void ClearGrid()
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    cells[x, y] = null;
+                }
+            }
+        }
+        public void PrintGrid()
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    Console.Write(cells[x, y]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public void innitializeGrid(string defaultValue)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    cells[x, y] = defaultValue;
+                }
+            }
+        }
 
     }
 }
--- neah/neah/Program.cs
+++ neah/neah/Program.cs
@@ -4,22 +4,12 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Grid grid = new Grid(3, 2);
-        grid.SetCell(0, 0, "A");
-        grid.SetCell(1, 0, "B");
-        grid.SetCell(2, 0, "C");
-        grid.SetCell(0, 1, "D");
-        grid.SetCell(1, 1, "E");
-        grid.SetCell(2, 1, "F");
-        for (int y = 0; y < grid.Height; y++)
-        {
-            for (int x = 0; x < grid.Width; x++)
-            {
-                Console.Write(grid.GetCell(x, y) + " ");
-            }
-            Console.WriteLine();
-        }
+        
+        Grid grid = new Grid(8, 8);
+        grid.innitializeGrid("{}");
+        grid.PrintGrid();
         Console.ReadLine();
     }
     
+
 }
