916f90872d734a0e13afb93b44694b413b529421
Harry Duff <duffhj@kingschester.co.uk>
2025-12-11 11:04:42 +0000

    added some comments

--- neah/neah/entetys/Ant.cs
+++ neah/neah/entetys/Ant.cs
@@ -12,7 +12,9 @@ namespace neah.entetys
         { 
 
         }
-        
+        public int clamedClassid { get; set; } = -1;
+        // -1 means no class claimed
+        // add stuff for queue so once ant has clamed a task it wont claim another till done
         public override int Id { get; set; }
         public override char Species { get; set; } = 'N';
         public override string Symbol { get; set; } = "[N]";
