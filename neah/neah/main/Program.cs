7935638878c430803bb01751b23219ef4d920470
Harry Duff <duffhj@kingschester.co.uk>
2025-12-12 11:05:10 +0000

    attempted to add so u can change size of grid

--- neah/neah/main/Program.cs
+++ neah/neah/main/Program.cs
@@ -4,7 +4,12 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        Console.WriteLine("Neah Ant Colony Simulation");
+        Console.WriteLine("--------------------------");
+        Console.WriteLine("enter width then height of sim");
+        int width = Convert.Toint32( Console.ReadLine());
+        ConvertToInt32  converttoint = new ConvertToInt32();
+        Console.ReadLine();
         Game game = new Game(15, 20);
         game.Initialize_Game();
         
